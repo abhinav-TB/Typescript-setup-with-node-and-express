14f7f473ae0d66443311a9a20d1cdd30c9ed377e
abhinav-TB <abhinavtb@gmail.com>
2020-05-12 22:53:00 +0530

    basic setup done

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log('test');
+import express from "express"
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send('Hello');
+
+});
+
+app.listen(5000,()=>{console.log('server running a')})
